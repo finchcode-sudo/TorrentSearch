3703e74240acedb0a7f1f454e67038a33bce3245
finchcode-sudo <poqq26566@gmail.com>
2026-09-08 08:37:55 +0800

    Enable MyPornClub provider by default

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/MyPornClub.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/MyPornClub.kt
@@ -25,7 +25,7 @@ class MyPornClub(private val networkClient: NetworkClient) :
     override val url = "https://myporn.club"
     override val supportedCategories = setOf(Category.Porn)
     override val safetyStatus = SearchProviderSafetyStatus.Safe
-    override val enabledByDefault = false
+    override val enabledByDefault = true
 
     private val resultsPageParser = MyPornClubResultsPageParser(name, networkClient)
 
